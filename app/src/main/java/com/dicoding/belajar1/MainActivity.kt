ce6e4f18139a779c3d68e5cd763a970728c0503a
redVELVET <rahmat.abdianto2@gmail.com>
2024-04-04 21:17:36 +0800

    Initial commit

---
+++ app/src/main/java/com/dicoding/belajar1/MainActivity.kt
@@ -0,0 +1,77 @@
+package com.dicoding.belajar1
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    companion object {
+        private const val STATE_RESULT = "state_result"
+    }
+
+    private lateinit var edtWidth: EditText
+    private lateinit var edtHeight: EditText
+    private lateinit var edtLength : EditText
+    private lateinit var btnCalculate: Button
+    private lateinit var tvResult: TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        edtWidth = findViewById(R.id.edt_width)
+        edtHeight = findViewById(R.id.edt_height)
+        edtLength = findViewById(R.id.edt_length)
+        btnCalculate = findViewById(R.id.btn_calculate)
+        tvResult = findViewById(R.id.tv_result)
+        btnCalculate.setOnClickListener(this)
+
+        if (savedInstanceState != null){
+            val result = savedInstanceState.getString(STATE_RESULT)
+            tvResult.text = result
+        }
+
+    }
+
+
+
+
+    override fun onClick(view: View?) {
+        if (view?.id == R.id.btn_calculate) {
+            val inputLength = edtLength.text.toString().trim()
+            val inputWidth = edtWidth.text.toString().trim()
+            val inputHeight = edtHeight.text.toString().trim()
+
+            var isEmptyFields = false
+            if (inputLength.isEmpty()){
+                isEmptyFields = true
+                edtLength.error = "tidak boleh kosong"
+            }
+
+            if (inputWidth.isEmpty()) {
+                isEmptyFields = true
+                edtWidth.error = "tidak boleh kosong"
+            }
+
+            if (inputHeight.isEmpty()){
+                isEmptyFields = true
+                edtHeight.error = "tidak boleh kosong"
+            }
+
+            if (!isEmptyFields){
+
+                val volume = inputLength.toDouble() * inputWidth.toDouble() * inputHeight.toDouble()
+                tvResult.text = volume.toString()
+
+            }
+
+
+        }
+
+    }
+}
